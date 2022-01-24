19ef4eabeb4e4e598edce081961a96e27a2aaaee
luke <lukeledvina19@gmail.com>
2022-01-23 23:08:35 -0500

    Add fix to divide by zero

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return Math.round((a / b) * 100) / 100;
+    return Math.round((a / b) * 100000) / 100000;
 }
 
 function operate(operator, a, b) {
@@ -33,6 +33,8 @@ calcButtons.forEach(element => element.addEventListener('click', () => {
     
 
     // Breaks when another number is input after operate is called, instead of immediatly being another operator
+    // operators and equals break equation if input before first number
+    // equals breaks if input before both numbers are entered (wording here might mess you up)
 
     if(element.textContent === '+' || element.textContent === '-' || element.textContent === 'x' || element.textContent === '/') {
         
@@ -41,9 +43,15 @@ calcButtons.forEach(element => element.addEventListener('click', () => {
     } else if(element.textContent === '=' && firstValue !== '' && secondValue !== ''){
         firstValue = parseFloat(firstValue);
         secondValue = parseFloat(secondValue);
-        display.textContent = operate(calcOperation, firstValue, secondValue);
-        firstValue = display.textContent;
+        if(secondValue == 0 && calcOperation === '/') {
+            clearCalc();
+            display.textContent = 'lol'; 
+        } else {
+            display.textContent = operate(calcOperation, firstValue, secondValue);
+            firstValue = display.textContent;   
+        }
         secondValue = '';
+        
     } else if(!firstNumber) {
         secondValue += element.textContent;
         display.textContent = secondValue;
@@ -57,11 +65,14 @@ calcButtons.forEach(element => element.addEventListener('click', () => {
     
 }));
 
-let clearButton = document.querySelector('.clear');
-clearButton.addEventListener('click', () => {
+function clearCalc() {
     firstValue = '';
     firstNumber = true;
     secondValue = '';
     calcOperation = '';
     display.textContent = 'Enter a number:';
-})
+}
+
+let clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', clearCalc);
+
